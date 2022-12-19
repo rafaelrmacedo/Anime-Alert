7be6e4bc77c767d7cbced22d2ffa2bba20150204
rafaelrmacedo <rafaelrodriguesmacedo2004@gmail.com>
2022-12-19 12:02:24 -0300

    first Spring files added

---
+++ src/main/java/AnimeAlert/animeAlert/AnimeAlertApplication.java
@@ -0,0 +1,13 @@
+package AnimeAlert.animeAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AnimeAlert/animeAlert/AnimeAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package AnimeAlert.animeAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
